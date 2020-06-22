47469b5126cf65384e44a56ecfcf1756ccf3bc9a
Edaz <edazpotato@gmail.com>
2020-06-22 14:10:58 +1200

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ let redirects = [
     "code": "no",
     "url": "https://edazpotato.github.io/NO/"
   }
-]
+];
 
 app.get('/', function (req, res) {
-  
+  if (redirects[{"code":code}]) {
+    let link = redirects[{"code":code}].url;
+    res.redirect(link);
+  } else {
+    res.send("Error 404: Redirect not found");
+  }
 })
