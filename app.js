09b87c8f7c53ea058dd58b7433dbbb122712b405
Edaz <edazpotato@gmail.com>
2020-06-22 14:08:20 +1200

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+let redirects = [
+  {
+    "code": "no",
+    "url": "https://edazpotato.github.io/NO/"
+  }
+]
+
+app.get('/', function (req, res) {
+  
+})
